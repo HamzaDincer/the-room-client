3f8aef8bc4f1b6a44cbeba4cd27c5374c2afefa5
Hamza Dincer <hamzamustakdincer@gmail.com>
2023-04-12 10:28:38 -0400

    refactor the app js

--- src/App.js
+++ src/App.js
@@ -1,185 +1,77 @@
-import { useEffect, useRef, useState } from 'react';
-import io from 'socket.io-client';
-import Peer from 'peerjs';
+import React, { useEffect, useRef, useState } from 'react';
 import useSpeechToText from 'react-hook-speech-to-text';
-
 import './App.scss';
+import VideoGrid from './components/VideoBox';
+import { socket, peer } from './socket';
+
 
 function App() {
 
-  const [roomId, setRoomId] = useState(123);
-  const [userId, setUserId] = useState("");
-  const [caption, setCaption] = useState(null)
-  const socketRef = useRef(null);
-  const videoGridRef = useRef(null);
-  
+  const [myVideoStream, setMyVideoStream] = useState(null);
 
-  const {
-    error,
-    interimResult,
-    isRecording,
-    results,
-    startSpeechToText,
-    stopSpeechToText,
-  } = useSpeechToText({
-    continuous: true,
-    useLegacyResults: false,
-    lang: "en-US",
-  });
+  // useEffect(() => {
+
+  //   async function enableStream() {
+  //     try{
+  //       navigator.mediaDevices.getUserMedia({
+  //           video: true,
+  //           audio: true
+  //       }).then(stream => {
+  //         setMyVideoStream(stream);
+  //         console.log("Log: your Video Stream has loaded.");
+  //       });
+  //     }catch(err){
+  //       console.log("Error: " + err);
+  //     }
+  //   }
+
+  //   if (!myVideoStream) {
+  //     enableStream();
+  //   }else{
+  //     console.log("Log: your Video Stream not loaded.");
+  //   }
 
 
+  // }, []);
 
-  useEffect(() => {
 
-    socketRef.current = io("https://e096d04e78dc.ngrok.app");
-
-    socketRef.current.on('room-created', (roomId) => {
-      setRoomId(roomId);
-    });
-
-    socketRef.current.on('caption-update', (roomId, userId, caption) => {
-      const captionElement = document.getElementById(`caption`);
-      captionElement.textContent = caption;
-    });
-    socketRef.current.emit('caption', roomId, userId, caption);
-
-  
-    const myPeer = new Peer();
-
-    const videoGrid = videoGridRef.current; 
-  
-    myPeer.on('open', (id) => {
-      setUserId(id);
-      if (roomId) {
-        socketRef.current.emit('join-room', roomId, id);
-      }
-    });
-  
+  if(!myVideoStream){
+                        
     navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true
-    })
-    .then((stream) => {
-      const myVideo = document.createElement('video');
-      myVideo.classList.add('col-3');
-      myVideo.muted = true;
-      addVideoStream(myVideo, stream);
-  
-      myPeer.on('call', (call) => {
-        call.answer(stream);
-  
-        const video = document.createElement('video');
-        video.classList.add('col-3');
-        video.id = call.peer;
-        call.on('stream', (userVideoStream) => {
-          addVideoStream(video, userVideoStream);
-        });
-        call.on("error", (err) => {
-          console.log(err);
-        });
-      });
-  
-      socketRef.current.on('user-connected', (userId) => {
-        addMessage(`New user (${userId}) connected to the room.`);
-        connectToNewUser(userId, stream);
-      });
-  
-      socketRef.current.on('user-disconnected', (userId) => {
-        addMessage(`A user (${userId}) disconnected from the room.`);
-        document.getElementById(userId).remove();
-      });
+        video: true,
+        audio: true
+    }).then(stream => {
+      setMyVideoStream(stream);
+      console.log("Video stream has loaded.");
     });
-  
-    function connectToNewUser(userId, stream) {
-      const call = myPeer.call(userId, stream);
-      const video = document.createElement('video');
-      video.classList.add('col-3');
-      video.id = userId;
-    
-      call.on('stream', (userVideoStream) => {
-        addVideoStream(video, userVideoStream);
-      });
-    
-      call.on("error", (err) => {
-        console.log(err);
-      });
-      
-      // Add the new video element to the video grid
-      videoGridRef.current.append(video);
-    }
-  
-    function addVideoStream(video, stream) {
-      video.srcObject = stream;
-      video.addEventListener('loadedmetadata', () => {
-        video.play();
-      });
-    
-      const videoContainer = document.createElement('div');
-      videoContainer.classList.add('video-container');
-      videoContainer.appendChild(video);
-    
-      const videoCaption = document.createElement('div');
-      videoCaption.classList.add('video-caption');
-      videoCaption.id = `caption`;
-      videoContainer.appendChild(videoCaption);
-    
-      videoGrid.appendChild(videoContainer);
-    
-      if (!isRecording) {
-        startSpeechToText();
-      }
-    
-      const newCaption = results[results.length - 1];
-      setCaption(newCaption);
-    
-      const captionElement = videoCaption;
-      if (newCaption && newCaption.transcript) {
-        captionElement.textContent = newCaption.transcript;
-    
-        // Emit the updated caption to the server
-        socketRef.current.emit('caption', roomId, userId, newCaption.transcript);
-      }
-    }
-    
-  
-    function addMessage(msgtxt) {
-      const chatbox = document.getElementById("chatbox");
-      const message = document.createElement("p");
-      message.innerHTML = msgtxt;
-      chatbox.append(message);
-    }
-  
-    return () => {
-      socketRef.current.disconnect();
-      myPeer.disconnect();
-    }
-  }, []);
+  }
 
-  useEffect(() => {
-
-    const newCaption = results[results.length - 1];
-    console.log(results);
-    setCaption(newCaption);
-
-    const captionElement = document.getElementById(`caption`);
-    if (newCaption && newCaption.transcript) {
-      captionElement.textContent = newCaption.transcript;
-    }
-  }, [results]);
 
   return (
     <>
-      <h1>Hamza's Room </h1>
-      <div className="container">
-        <div className="row">
-          <div className="col-7">
-            <div id="video-grid" className="row" ref={videoGridRef}>
+      <div id="header">
+        <h1>Room - <span id="roomID"></span></h1>
+        <h2>User: <span id="userID"></span></h2>
+      </div>
+      <div id="main">
+          <div id="video-container">
+            
+            <VideoGrid myVideoStream={myVideoStream} />
+            
+            <div id="video-controls">
+
             </div>
+
           </div>
-        </div>
-        <div className="col-5">
-            <div id="chatbox">
+
+          <div id="chat-container" className="chat">
+            <div id="chat-screen">
+              <p>Lorem ipsum dolar sit amet</p>
             </div>
+            <div id="chat-input">
+
+            </div>
+          
           </div>
       </div>
     </>
