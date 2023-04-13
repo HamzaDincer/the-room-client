af322c114a8c9531fe45bfa246b9cb830f4d9d32
Hamza Dincer <hamzamustakdincer@gmail.com>
2023-04-13 01:11:07 -0400

    change the structure

--- src/socket.js
+++ src/socket.js
@@ -2,8 +2,8 @@ import { io } from 'socket.io-client';
 import Peer from 'peerjs';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : "http://localhost:8000";
+const URL = process.env.NODE_ENV === 'production' ? undefined : "https://e096d04e78dc.ngrok.app";
 
-export const socket = io("https://e096d04e78dc.ngrok.app");
+export const socket = io(URL);
 
 export const peer = new Peer() ;
