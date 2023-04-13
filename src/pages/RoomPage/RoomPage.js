0615ae6f30dc65589f60c51d9c910b88410fcd9a
Hamza Dincer <hamzamustakdincer@gmail.com>
2023-04-13 00:24:59 -0400

    edited styling of roompage

--- src/pages/RoomPage/RoomPage.js
+++ src/pages/RoomPage/RoomPage.js
@@ -4,10 +4,7 @@ import './RoomPage.scss';
 import VideoGrid from '../../components/VideoBox/VideoBox';
 import { socket, peer } from '../../socket';
 import logo from "../../assets/TheRoom.jpeg"
-import {Link} from 'react-router-dom'
-
-
-
+import { Link, useLocation } from 'react-router-dom';
 
 function RoomPage() {
 
@@ -15,12 +12,20 @@ function RoomPage() {
   const [messages, setMessages] = useState([]);
   const [transcription, setTranscription] = useState('');
   const [isAccessGranted, setIsAccessGranted] = useState(false);
-  
+
+  const location = useLocation();
+
+  let userName = 'Guest';
+
+  if (location.state) {
+    userName = location.state.userName;
+  }
 
   const handleSend = () => {
     const message = document.getElementById("chat-input").value;
     console.log(message);
-    const name = "John";
+    const name = userName || "Guest";
+    console.log(location.state)
     socket.emit("chat-message", { message, name });
     document.getElementById("chat-input").value = "";
     setMessages(prevMessages => [...prevMessages, { name, message }]);
@@ -49,11 +54,10 @@ function RoomPage() {
   return (
     <>
       <div id="header">
-      <h1>Hamza's Room <span id="roomID"></span></h1>
-      <Link to='/'>
-      <img className="homepage__logo" src={logo} alt="TheRoom Logo" />
-      </Link>
-
+        <h1>Hamza's Room <span id="roomID"></span></h1>
+        <Link to='/'>
+          <img className="homepage__logo" src={logo} alt="TheRoom Logo" />
+        </Link>
       </div>
       <div id="main">
           <div id="video-container">
@@ -67,25 +71,24 @@ function RoomPage() {
           </div>
 
           <div id="chat-container" className="chat">
-          <div id="chat-screen">
-            {messages.map((message, index) => (
-              <div
-                key={index}
-                className={`message-container ${
-                  message.name === "John" ? "message-right" : "message-left"
-                }`}
-              >
-                <strong>{message.name}: </strong>
-                {message.message}
-              </div>
-            ))}
+            <div id="chat-screen">
+              {messages.map((message, index) => (
+                <div
+                  key={index}
+                  className={`message-container ${
+                    message.name === userName ? "message-right" : "message-left"
+                  }`}
+                >
+                  <strong>{message.name}: </strong>
+                  {message.message}
+                </div>
+              ))}
+            </div>
+            <div className='chat__input'>
+              <input type="text" id="chat-input" placeholder="Type your message here" />
+              <button id="send-btn" onClick={handleSend}>Send</button>
+            </div>
           </div>
-          <div className='chat__input'>
-          <input type="text" id="chat-input" placeholder="Type your message here" />
-          <button id="send-btn" onClick={handleSend}>Send</button>
-          </div>
-        </div>
-        
       </div>
     </>
   );
